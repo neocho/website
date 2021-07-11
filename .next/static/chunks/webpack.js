fc4cddbe77a9afb99398e2d0d3ec97ce610536c9
Neo <choneo.ch@gmail.com>
2021-07-10 20:03:25 -0400

    builddd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0537918275445e9221df";
+/******/ 	var hotCurrentHash = "6e3763a2278387a6630c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/vibe.js
+++ pages/vibe.js
@@ -6,7 +6,7 @@ export default function Vibe() {
     const styles = {
         border: '10px solid #000000',
         borderRadius: '3%',
-        width: '410px',
+        width: '350px',
         height: '400px'
     };
 
@@ -14,21 +14,16 @@ export default function Vibe() {
 		<div className="place-items-center min-h-screen">
 			<div className="ml-2">
 				<Header title={"Vibe"}/>
+                
+                <div className="ml-4 mr-4 flex flex-col w-full">
 
-
-                <div className="ml-4 flex flex-col w-full">
-
-                    <h1 className="text-base text-lg font-semibold ml-3 mb-3 mt-5">Neo's Drawing Pad 🎨</h1>
+                    <h1 className="text-base text-3xl font-semibold ml-3 mb-3 mt-3">🎨</h1>
                     
                     <ReactSketchCanvas
                         style={styles}
                         strokeWidth={3}
                         strokeColor="black"
                     />
-
-                    <div className="flex mt-5 space-x-3">
-                       
-                    </div>
                 </div>
                     
 				<Footer />
